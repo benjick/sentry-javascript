800a0b5d756adc6a0e2b90800457b00dee2dacdf
Francesco Novy <francesco.novy@sentry.io>
2024-12-17 10:05:16 +0100

    better test

--- dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/test.ts
@@ -41,6 +41,7 @@ describe('errors in TwP mode have same trace in trace context and getTraceData()
 
           const traceData = contexts?.traceData || {};
 
+          expect(traceData['sentry-trace']).toMatch(/^[a-f0-9]{32}-[a-f0-9]{16}$/);
           expect(traceData['sentry-trace']).toContain(`${trace_id}-`);
           // span_id is a random span ID
           expect(traceData['sentry-trace']).not.toContain(span_id);
@@ -48,6 +49,7 @@ describe('errors in TwP mode have same trace in trace context and getTraceData()
           expect(traceData.baggage).toContain(`sentry-trace_id=${trace_id}`);
           expect(traceData.baggage).not.toContain('sentry-sampled=');
 
+          expect(traceData.metaTags).toMatch(/<meta name="sentry-trace" content="[a-f0-9]{32}-[a-f0-9]{16}">/);
           expect(traceData.metaTags).toContain(`<meta name="sentry-trace" content="${trace_id}-`);
           // span_id is a random span ID
           expect(traceData.metaTags).not.toContain(span_id);
